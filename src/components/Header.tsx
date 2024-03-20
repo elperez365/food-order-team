678ade3e52540ed28d1f15bc12fa110e4e9423f3
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-20 14:47:11 +0100

    Add styling to header + navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,16 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="relative flex w-full justify-between bg-red-500 px-8 py-4">
-      <section className="flex items-center justify-center gap-4 bg-slate-400">
+    <header className="relative flex w-full justify-between bg-red-500">
+      <section className="flex flex-[3] items-center justify-start gap-4 bg-slate-400 p-4">
         <img
           src="/vite.svg"
           alt="logo"
-          className="object-container aspect-square h-full w-20 rounded-full border border-white"
+          className="aspect-square h-20 w-20 max-w-none rounded-full border border-white object-contain"
         />
-        <h1 className="text-6xl text-white drop-shadow-md">React Food</h1>
+        <h1 className="text-[clamp(1.6rem,2vw+1rem,3.75rem)] font-semibold uppercase text-white drop-shadow-md">
+          Re-commerce
+        </h1>
       </section>
       <Navbar />
     </header>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <ul className="flex flex-1 items-center justify-between gap-4 bg-blue-200">
+    <ul className="flex flex-1 items-center justify-center gap-4 bg-blue-200 p-4 text-3xl font-semibold uppercase text-white drop-shadow-md">
       <li>History - ()</li>
       <li>Cart - ()</li>
     </ul>

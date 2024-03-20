6d1c2a2eceea94a62919b6619c8abe58ed042f95
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-20 12:45:35 +0100

    scaffoldig init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.css";
-import { useAppSelector, useAppDispatch } from "./redux/hooks";
-import { increment } from "./redux/slices/counterSlice";
+import Navbar from "./components/Navbar";
+import Products from "./components/Products";
 
 function App() {
-  const counter = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
   return (
     <>
-      <p className="text bg-red-500">{counter}</p>
-      <button onClick={() => dispatch(increment())}>INCREMENTA</button>
-      <p>ho fatto una modific di prova</p>
-      <p>ciao a tutti - fede</p>
+      <Navbar />
+      <Products />
     </>
   );
 }

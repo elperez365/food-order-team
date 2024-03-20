5f51d1605d416d0026cf55ac8607cb581b0c788b
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-20 11:53:11 +0100

    tailwind, redux and prettierconfig init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { useAppSelector, useAppDispatch } from "./redux/hooks";
+import { increment } from "./redux/slices/counterSlice";
 
 function App() {
+  const counter = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <p>Ciao</p>
+      <p className="text bg-red-500">{counter}</p>
+      <button onClick={() => dispatch(increment())}>INCREMENTA</button>
     </>
   );
 }

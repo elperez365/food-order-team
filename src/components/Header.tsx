0d277c8168177c87210fedfb6ea1c6ef3508800a
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-20 15:17:12 +0100

    Improve responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Title from "./Title";
 const Header = () => {
   return (
-    <header className="relative flex w-full flex-wrap justify-between bg-black/55">
+    <header className="max-[350] relative flex w-full flex-wrap justify-between bg-black/55 max-[400px]:justify-center">
       <Title />
       <Navbar />
     </header>
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -6,7 +6,7 @@ export default function NavLinks({ className }: NavLinksProps) {
   return (
     <>
       <ul
-        className={`${className ? className + " " : ""} items-center justify-center p-4 text-3xl font-semibold uppercase text-white drop-shadow-md [&>*]:cursor-pointer [&>*]:p-4`}
+        className={`${className ? className + " " : ""} items-center justify-center p-4 text-3xl font-semibold uppercase text-white drop-shadow-md [&>*]:cursor-pointer [&>*]:p-4 [&_li]:text-[clamp(1rem,1vw+1rem,2rem)]`}
       >
         <li>History - ()</li>
         <li>Cart - ()</li>

8fbde9245828165c950a4cc4e48a6ef3f37b1a57
Thomas Armao <thomas.armao.93@gmail.com>
2024-03-21 14:12:48 +0100

    products

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,26 +10,40 @@ type ProductCardProps = {
 
 const ProductCard = ({
   image = "https://cdn.dummyjson.com/product-images/30/1.jpg",
-  title = "titolo",
+  title = "Titolo",
   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mi erat, sollicitudin non interdum efficitur, malesuada sed diam.",
   category = "categoria",
-  price = 27,
+  price = 27.23,
 }: ProductCardProps) => {
   return (
-    <div className="w-5/6 md:w-1/4 m-auto border border-gray-100 shadow-sm rounded-md">
-      <div>
-        <img className="w-full rounded-t-md" src={image} alt={title} />
+    <div className="m-auto flex w-5/6 flex-col rounded-xl border border-gray-100 bg-gray-50 p-4 shadow-xl md:h-[650px] md:w-[350px]">
+      <div className="h-[350px] w-full rounded-md bg-gray-300 md:h-2/3">
+        <img
+          className="h-full w-full rounded-md object-contain"
+          src={image}
+          alt={title}
+        />
       </div>
       <div className="p-2">
-        <h3 className="font-bold text-xl">{title}</h3>
-        <span className="text-xs text-white bg-[#7b2d26] px-2 py-1 rounded-sm font-thin">{category}</span>
+        <div className="flex justify-between">
+          <h3 className="text-2xl font-bold text-gray-900 md:text-lg">
+            {title}
+          </h3>
+          <span className="text-xl font-semibold text-gray-900 md:text-lg">
+            € {price}
+          </span>
+        </div>
+        <span className="rounded-md bg-[#7b2d26] px-2 py-1 text-xs font-thin text-white">
+          {category}
+        </span>
       </div>
-      <div className="p-2">
-        <p className="text-base">{description}</p>
+      <div className="p-2 md:h-1/6">
+        <p className="... overflow-hidden text-ellipsis text-base text-gray-600">
+          {description}
+        </p>
       </div>
-      <div className="p-2 flex justify-between">
-        <span className="text-xl font-semibold">€ {price}</span>
-        <Button text="Add to cart" style="classic" />
+      <div className=" flex w-full justify-between p-2">
+        <Button text="Add to cart" style="classicFullSize" />
       </div>
     </div>
   );

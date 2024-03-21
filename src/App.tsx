593f8fa6d9675cd2a74a50077715be4026ab1f76
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-21 12:48:27 +0100

    checkout layout init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import CheckOut from "./components/Layout/CheckOut";
 
 import Products from "./components/Products";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <CheckOut />
       <Products />
     </>
   );
---
+++ src/components/Layout/StatusOrder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StatusOrder: React.FC = () => {
+  return (
+    <div>
+      <h1>Order Success</h1>
+      <p>Your order has been successfully placed</p>
+    </div>
+  );
+};
+
+export default StatusOrder;

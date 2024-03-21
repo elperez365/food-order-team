8a5e316dfe625c482a5912258d19acb0d73229d7
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-21 09:57:23 +0100

    useFetch init

---
+++ src/components/Hooks/useFetch.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+
+export default function useFetch(
+  fetcher: Function,
+  sliceName: string,
+  action: Function,
+) {
+  const data = useAppSelector(
+    (state: { [key: string]: any }) => state[sliceName],
+  );
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetcher()
+      .then((data: any) => {
+        dispatch(action(data));
+        setLoading(false);
+      })
+      .catch((error: any) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return { data, loading, error };
+}

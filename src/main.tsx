dc99b7f3ae1cf91d717a9128de043a938b3ae462
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-21 17:34:29 +0100

    toast position bottom center

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer />
+      <ToastContainer position="bottom-center" />
       <App />
     </Provider>
   </React.StrictMode>,

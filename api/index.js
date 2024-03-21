69aed3ca1f2046c6b8f1a548afe452c027c700c2
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-21 09:38:48 +0100

    fix cors

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/products", async (req, res) => {
   try {
     const products = await fs.readFile("./api/products.json", "utf8");

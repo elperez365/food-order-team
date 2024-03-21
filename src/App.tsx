9ca530bfe702352bdbd420ba18bc24134668ce2b
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-21 12:49:04 +0100

    checkout init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
-import CheckOut from "./components/Layout/CheckOut";
-
 import Products from "./components/Products";
 
 function App() {
   return (
     <>
       <Header />
-      <CheckOut />
       <Products />
     </>
   );

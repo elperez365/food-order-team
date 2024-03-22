2ab5cd4041b1337bb510c1beaa9e18cae557292c
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-22 14:40:18 +0100

    Refactor title navbar

--- src/components/HeaderComponents/Title.tsx
+++ src/components/HeaderComponents/Title.tsx
@@ -2,14 +2,14 @@ import icon from "../../assets/icon.png";
 export default function Title() {
   return (
     <>
-      <section className="flex items-center justify-start gap-4 p-4">
+      <section className="flex items-center justify-start p-4">
         <img
           src={icon}
           alt="logo"
           className=" drop-shadow-white h-16 w-16 max-w-none object-contain"
         />
-        <h1 className="text-nowrap text-[clamp(1.6rem,2vw+1rem,3.75rem)] font-semibold uppercase text-white drop-shadow-md max-md:hidden">
-          Re-commerce
+        <h1 className="text-nowrap text-[clamp(1.2rem,1vw+1rem,2rem)] font-semibold uppercase text-white drop-shadow-md max-md:hidden">
+          e-commerce
         </h1>
       </section>
     </>

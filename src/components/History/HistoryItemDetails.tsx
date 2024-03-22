8ac2cbe1b66df6fec049dae7b77936d79f8f87c7
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-22 10:15:54 +0100

    Add item details history  component

--- src/components/History/HistoryItemDetails.tsx
+++ src/components/History/HistoryItemDetails.tsx
@@ -1,3 +1,24 @@
-export default function HistoryItemDetails() {
-  return <></>;
+type HistoryItemDetailsProps = {
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+export default function HistoryItemDetails({
+  name,
+  price,
+  quantity,
+}: HistoryItemDetailsProps) {
+  return (
+    <>
+      <div className="mb-2 grid grid-cols-[3fr,1fr] gap-4 border-b border-black/30 text-black/90">
+        <p>
+          {name} - ${price}
+        </p>
+        <p>
+          {quantity} - ${price * quantity}
+        </p>
+      </div>
+    </>
+  );
 }

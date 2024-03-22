0959bec15a5c26cb57fe798418f95741cb3a77a4
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-22 12:10:02 +0100

    Add key on History to make it refresh after product is sent to db

--- src/components/HeaderComponents/NavbarComponents/NavLinks.tsx
+++ src/components/HeaderComponents/NavbarComponents/NavLinks.tsx
@@ -37,7 +37,7 @@ export default function NavLinks({ className }: NavLinksProps) {
           Cart
         </NavLink>
         <Modal isOpen={currentView === "history"}>
-          <History />
+          <History key={currentView} />
         </Modal>
         <Modal
           isOpen={

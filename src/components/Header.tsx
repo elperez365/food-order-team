6273a14baee1cc5e0bce04df0b11b102c2db7c75
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-22 14:45:31 +0100

    Remove border to navbar add dropshadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from "./HeaderComponents/Navbar";
 import Title from "./HeaderComponents/Title";
 const Header = () => {
   return (
-    <header className="sticky top-0 z-20 mx-auto flex w-full flex-wrap justify-between border-b border-white bg-midnightGreen ">
+    <header className="sticky top-0 z-20 mx-auto flex w-full flex-wrap justify-between bg-midnightGreen shadow-xl shadow-antiFlash ">
       <Title />
       <Navbar />
     </header>
